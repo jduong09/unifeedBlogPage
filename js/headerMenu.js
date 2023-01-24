ac3c7a7f9cea0c8c1896708cfef3711ec21ac9e2
Justin Duong <jduong@umich.edu>
2023-01-24 17:21:42 -0500

    Finished responsive design for mobile, tablet and desktop view.

--- js/headerMenu.js
+++ js/headerMenu.js
@@ -11,4 +11,22 @@ document.addEventListener('DOMContentLoaded', () => {
       divNav.classList.add('hide');
     }
   });
+
+  if (window.innerWidth >= 767) {
+    imgNavHamburger.classList.add('hide');
+    divNav.classList.remove('hide');
+  } else {
+    imgNavHamburger.classList.remove('hide');
+    divNav.classList.add('hide');
+  }
+
+  window.addEventListener('resize', () => {
+    if (window.innerWidth >= 767) {
+      imgNavHamburger.classList.add('hide');
+      divNav.classList.remove('hide');
+    } else {
+      imgNavHamburger.classList.remove('hide');
+      divNav.classList.add('hide');
+    }
+  });
 });
